9b820867ed2aa289c437a5318841436e5b0b5b10
Nujsua Hawj <nousainther@gmail.com>
2022-08-11 14:09:44 +0700

    version 1.5

--- api/sanas.php
+++ api/sanas.php
@@ -9,7 +9,7 @@
     try{
         $conn = new PDO('mysql:host=localhost;dbname=u783615662_pos', 'u783615662_nujsua', '3mkaK6J$7j');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $conn->prepare('SELECT `product_variants`.`selling_price` AS sell, products.title AS name, products.description AS details, products.imageURL AS image FROM `product_variants` JOIN products ON `product_variants`.`product_id` = products.id WHERE products.category_id =1 AND product_variants.enabled=1;');
+        $stmt = $conn->prepare('SELECT `product_variants`.`selling_price` AS sell, products.title AS name, products.description AS details, products.imageURL AS image FROM `product_variants` JOIN products ON `product_variants`.`product_id` = products.id WHERE products.category_id =1 AND product_variants.enabled=1 ORDER BY products.id DESC;');
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($result);
